37b6932b254cf8e74bfcce5d26f8fadf86b5f17c
Masafumi Mochizuki <there_is_a_moon@yahoo.co.jp>
2018-10-27 06:31:48 +0900

    seeds modified.

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,36 +9,36 @@ def CreateUser(number)
   )
 end
 CreateUser(999999)
-CreateUser(222222)
+CreateUser(111111)
 
-# ■分類の作成
-def CreateCategory(string)
-  Category.create!(
-    name: string
-  )
-end
-CreateCategory("aiueo")
-CreateCategory("kkkkk")
-CreateCategory("oiuyt")
+# # ■分類の作成
+# def CreateCategory(string)
+#   Category.create!(
+#     name: string
+#   )
+# end
+# CreateCategory("aiueo")
+# CreateCategory("kkkkk")
+# CreateCategory("oiuyt")
 
-# ■言語の作成
-def CreateLanguage(string)
-  Language.create!(
-    name: string
-  )
-end
-CreateLanguage("C言語")
-CreateLanguage("shell script")
+# # ■言語の作成
+# def CreateLanguage(string)
+#   Language.create!(
+#     name: string
+#   )
+# end
+# CreateLanguage("C言語")
+# CreateLanguage("shell script")
 
-# ■ノウハウの作成
-def CreateKnowhow(string)
-  Knowhow.create!(
-    knowhow_class: false,
-    category_id: 1,
-    title: string,
-    content: string,
-    create_user_id: 1
-  )
-end
-CreateKnowhow("abc")
-CreateKnowhow("rrrrrrrrrrrrrrrrrrrrrrr")
+# # ■ノウハウの作成
+# def CreateKnowhow(string)
+#   Knowhow.create!(
+#     knowhow_class: false,
+#     category_id: 1,
+#     title: string,
+#     content: string,
+#     create_user_id: 1
+#   )
+# end
+# CreateKnowhow("abc")
+# CreateKnowhow("rrrrrrrrrrrrrrrrrrrrrrr")
